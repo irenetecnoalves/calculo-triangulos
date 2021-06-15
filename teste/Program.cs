6ecf66bfbb1669a50a2374bd7d5b9f7a3753644f
irenetecnoalves <irenetecnosenior@gmail.com>
2021-06-15 10:34:29 -0300

    Projeto para caluclos de triangulos

---
+++ teste/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+
+namespace teste
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            double a = double.Parse(s[0]);
+            double b = double.Parse(s[1]);
+            double c = double.Parse(s[2]);
+
+            //continue a solucao
+            if ((b > a) && (b > c) && (c < a))
+                Console.WriteLine("NAO FORMA TRIANGULO");
+
+            else if ((b > a) && (b < c) && (c > a))
+                Console.WriteLine("TRIANGULO RETANGULO");
+
+            else if ((a == b) && (b < c) && (c > a))
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+
+            else if ((b <= a) && (b <= c) && (c == a))
+                Console.WriteLine("TRIANGULO ACUTANGULO");
+
+            if ((a == b) && (b == c) && (c == a))
+                Console.WriteLine("TRIANGULO EQUILATERO");
+
+            if ((a >= b) && (b < c ) && (c>= a))
+
+                Console.WriteLine("TRIANGULO ISOSCELES");
+            
+            Console.ReadLine();
+        }
+    }
+}
+
